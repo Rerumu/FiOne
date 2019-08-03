1e2f9fcb888e559b3161ebf8b77685847c563438
Rerumu <keys3a@gmail.com>
2019-08-02 22:20:19 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# FiOne
+
+Pronounced like "Fiona" with an "e". This software aims to provide a decently fast and reliable way of executing Lua 5.1 bytecode under other Lua environments without the use of `load` or `loadstring`. For the purpose of anything from sandboxing to reimplementing arbitrary execution, this should serve your needs.
+
+Note that only an interpreter is provided, and compiled code must be obtained from some external source.
---
+++ example.lua
@@ -0,0 +1,26 @@
+local env = getfenv(0)
+local fio
+
+-- env.bit = require('bit').bit32 -- local testing
+
+local function wrap_func(fn)
+	return fio.wrap_lua(fio.stm_lua(string.dump(fn)), env)
+end
+
+fio = require('source')
+--fio = wrap_func(loadfile('source.lua'))() -- self running, uncomment to test
+
+local sum =
+	wrap_func(
+	function(num)
+		local ans = 0
+
+		for i = 1, num do
+			ans = ans + i
+		end
+
+		return ans
+	end
+)(100)
+
+print(sum)
